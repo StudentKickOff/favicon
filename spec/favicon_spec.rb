ce234697803e4d20387a780e9a51a1c99f1e5f71
benji <benjamin.cousaert@gmail.com>
2019-03-23 19:54:44 +0100

    [Favicon] Add small gem with favicon info

---
+++ spec/favicon_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Favicon do
+  it "has a version number" do
+    expect(Favicon::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
