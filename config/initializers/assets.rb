d352669253041a47e8da0a0e730afb9d7cf88a01
benji <benjamin.cousaert@gmail.com>
2019-09-14 19:47:21 +0200

    production tags

---
+++ config/initializers/assets.rb
@@ -0,0 +1,1 @@
+Rails.application.config.assets.precompile += %w( favicon-32x32.png favicon-16x16.png site.webmanifest safari-pinned-tab.svg favicon.ico browserconfig.xml )
--- lib/favicon.rb
+++ lib/favicon.rb
@@ -1,21 +1,9 @@
 require "favicon/version"
 
 module Favicon
-  def self.output_tags
-    tags = <<-HERE
-      <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
-      <link rel="manifest" href="/assets/site.webmanifest">
-      <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbad5">
-      <link rel="shortcut icon" href="/assets/favicon.ico">
-      <meta name="msapplication-TileColor" content="#da532c">
-      <meta name="msapplication-config" content="/assets/browserconfig.xml">
-      <meta name="theme-color" content="#ffffff">
-    HERE
-    tags.html_safe
-  end
-
   class Engine < ::Rails::Engine
+    initializer :assets do |config|
+      Rails.application.config.assets.paths << root.join("app", "assets", "images")
+    end
   end
 end
